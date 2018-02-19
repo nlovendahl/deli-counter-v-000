a6499e8c764322e5de76581af6e48c50f12ee3a4
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-19 19:52:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,9 @@ def line(deli)
     puts "The line is currently empty."
   else
   deli.each do |name|
+    line_list = []
     index = 1
-    puts "Welcome, #{name}. You are number #{index} in line."
+    line_list << "#{index}. #{name}"
     index += 1
   end
 end
