1a91698c65b22c3801a5e69f53d73a8f40fe7121
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-19 19:49:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,11 @@
 def line(deli)
+  if deli.length < 1
+    puts "The line is currently empty."
+  else
   deli.each do |name|
     index = 1
     puts "Welcome, #{name}. You are number #{index} in line."
     index += 1
   end
+end
 end
