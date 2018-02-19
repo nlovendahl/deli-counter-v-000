bf859cbc612796e0fadab853e4518eb667fb93c0
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-19 20:11:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,11 @@ def take_a_number(deli, name)
     deli << name
     puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
+
+def now_serving(deli)
+  if deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli.first}"
+  end
+end
