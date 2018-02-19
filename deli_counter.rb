65977d83583c0a3d676d06cb60ba9e6840357af4
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-19 20:05:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,11 @@ def line(deli)
   if deli.length < 1
     puts "The line is currently empty."
   else
-   puts "The line is currently: #{deli}"
+    line = []
+    count = 0
+    while count < deli.length
+      line << " #{count + 1}. #{deli[count]}"
+    end
+    puts "The line is currently: #{line}"
   end
 end
